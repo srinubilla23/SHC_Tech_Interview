89f5c88b64fba572fc9c7e550ae0a4de90e7b322
srinutoqqer <srinivas.kurakula@toqqer.com>
2020-01-07 18:59:59 +0530

    SHC Technologies Changes

--- src/angel_co/angel.java
+++ src/angel_co/angel.java
@@ -3,19 +3,14 @@ package angel_co;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.sikuli.script.Screen;
 
 public class angel 
